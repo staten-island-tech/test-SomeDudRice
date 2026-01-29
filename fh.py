26c32a957475a93b3a71cfd2c7974e65b92097d6
SomeDudRice <Lucasl197@nycstudents.net>
2026-01-29 10:11:34 -0500

    day 3

--- fh.py
+++ fh.py
@@ -1,20 +1,33 @@
 import turtle
 from turtle import *
 t = Turtle()
+# 1. Let's draw 1 rectangle witha width of 100px and a length of 125px.
+# def equal(x):
+#   t.left(90)
+#   t.forward(125)
+#   t.left(90)
+#   t.forward(100)
+#   t.left(90)
+#   t.forward(125)
+#   t.left(90)
+#   t.forward(100)
+# turtle.done()
 
-def equal(x):
-    t.forward(x)
-    t.left(120)
-    t.forward(x)
-    t.left(120)
-    t.forward(x)
-equal(200)
-def right():
-    t.forward(100)
-    t.left(90)
-    t.forward(100)
-    t.left(135)
-    t.forward(142)
-right()
 
-turtle.done()
+
+
+# 2. Draw 1 Triangle with 3 equal sides, length 90px.
+# def equal(x):
+#   t.left(120)
+#   t.forward(90)
+#   t.left(120)
+#   t.forward(90)
+#   t.left(120)
+#   t.forward(90)
+# equal(270)
+# turtle.done()
+
+#LOOPS IN PYTHON
+
+for i in range (3):
+    print(i)
