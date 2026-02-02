cc85de3ef674c22344f6cc044da5466c3f12b563
SomeDudRice <Lucasl197@nycstudents.net>
2026-02-02 10:17:20 -0500

    first of feburay

--- LoopFrootBootMootSloop.py
+++ LoopFrootBootMootSloop.py
@@ -13,18 +13,43 @@ t = Turtle()
 #     t.forward(100)
 #     t.left(90)
 
-def square():
- for i in range(60):
-    print (i)
-    t.forward(100)
-    t.right(90)
-    t.forward(100)
-    t.right(90)
-    t.forward(100)
-    t.right(90)
-    t.forward(100)
-    t.right(95)
-    
-square()
-turtle.done()
+# circle square vietnam flashback
+# def square():
+#  for i in range(60):
+#     print (i)
+#     t.forward(100)
+#     t.right(90)
+#     t.forward(100)
+#     t.right(90)
+#     t.forward(100)
+#     t.right(90)
+#     t.forward(100)
+#     t.right(95)
+# square()
+# turtle.done()
 
+# dont have to manually put in values
+# sidelength = 100
+# rotate = 90
+# def triangle(x,y):
+#     for i in range(4):
+#         t.forward(x)
+#         t.left(y)
+# triangle(100,90)
+
+
+def Squarespiral():
+    for i in range (60):
+        def square ():
+            length == 5
+            t.forward(length)
+            t.right(90)
+            t.forward(length)
+            t.right(90)
+            t.forward(length)
+            t.right(90)
+            t.forward(length)
+            t.right(95)
+            length = length + 5
+        square()
+Squarespiral()
